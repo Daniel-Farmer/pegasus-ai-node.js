22aae8fd4cb0e080e4ed21401b52e0ec58c1fe9d
Daniel Edward Farmer <67203566+Daniel-Farmer@users.noreply.github.com>
2024-12-14 03:29:44 +0000

    Delete public/css/chat.js

--- public/css/chat.js
+++
@@ -1,7 +0,0 @@
-export function displayMessage(sender, message) {
-    // ... (implementation details not provided in the previous context)
-}
-
-export function initChat() {
-    // ... (implementation details not provided in the previous context)
-}
